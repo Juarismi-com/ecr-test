bf3af229e854e0625d15cca4eb73632ffe1347c8
Ivan Fretes <vanfretes@gmail.com>
2025-12-10 14:18:54 -0300

    chore: aws cloudwatch

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js Docker container :)!\n');
+  res.end('Hello from Node.js Docker container xD!\n');
 });
 
 server.listen(port, hostname, () => {
